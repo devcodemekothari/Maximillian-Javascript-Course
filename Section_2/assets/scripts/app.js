1700d67b852ef3c5ff783576f1a18978665a579e
Dev Kothari <thisisdevkothari@gmail.com>
2022-01-21 21:23:59 +0530

    Adding Add button Eevent Listener

--- Section_2/assets/scripts/app.js
+++ Section_2/assets/scripts/app.js
@@ -1,1 +1,15 @@
-alert('This is an alert');
+let defaultResult = 0;
+let currenResult = defaultResult;
+
+function getUserInput() {
+    return parseInt(userInput.value);
+}
+
+function add(){
+    const enteredNumber = getUserInput();
+    const calcDescription = `${currenResult} + ${enteredNumber}`;
+    currenResult = currenResult + enteredNumber;
+    outputResult(currenResult, calcDescription);
+}
+
+addBtn.addEventListener('click',add);
